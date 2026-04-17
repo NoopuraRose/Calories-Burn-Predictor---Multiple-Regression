5409da3c65ef7cb8da4ccc8da7bc9a868d0ad77a
NoopuraRose <noopura.rose@gmail.com>
2026-04-16 18:30:28 -0700

    project completed

---
+++ predictor.py
@@ -0,0 +1,25 @@
+# library imports
+import pandas as pd 
+from sklearn.linear_model import LinearRegression
+
+# data set reading
+mydata = pd.read_csv("Calories_Burn_Prediction.csv")
+x = mydata[["duration", "heart_rate", "age"]]
+y = mydata[["calories"]]
+
+# model creation and training
+model = LinearRegression()
+model.fit(x,y)
+
+cf = model.coef_
+print("Coefficient = ", cf)
+intercept = model.intercept_
+print("Intercept = ", intercept)
+
+# predicting new value with use input
+duration = int(input("Enter the duration:"))
+heart_rate = int(input("Enter the heart rate:"))
+age = int(input("Enter you age:"))
+
+new_calories = model.predict([[duration, heart_rate, age]])
+print("Predicted calories = ", new_calories)
